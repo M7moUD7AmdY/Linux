ae7d701cc6cb8a86378ad5559c9763b306e2e22d
M7moUD7AmdY <128413333+M7moUD7AmdY@users.noreply.github.com>
2023-05-08 23:26:05 +0300

    Add files via upload

---
+++ 1-python/2-Labs/Lab4.py
@@ -0,0 +1,12 @@
+se1,se2,se3=input("Enter ur reading:").split(" ",3)
+mylist=[int(se1),int(se2),int(se3)]
+mytuple=(int(se1),int(se2),int(se3))
+mydict={
+    "Read1":se1,
+    "Read2":se2,
+    "Read3":se3
+
+}
+print(mylist)
+print(mytuple)
+print(mydict)
