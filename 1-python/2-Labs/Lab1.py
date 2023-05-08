e62e923747b9cfb4b0e388426a00df62757ee181
M7moUD7AmdY <128413333+M7moUD7AmdY@users.noreply.github.com>
2023-05-08 18:08:24 +0300

    Add files via upload

---
+++ 1-python/2-Labs/Lab1.py
@@ -0,0 +1,4 @@
+print("Name:Mahmoud Hamdi")
+print("DOB:1/5/1998")
+print("Faculty of Engineering Mansoura Universty")
+print("Graduation year 2021")
---
+++ 1-python/2-Labs/Lab2.py
@@ -0,0 +1,4 @@
+MyNamy=input("Enter your name:")
+MyAge=input("Enter your Age:")
+MyFaculty=input("Enter your Faculty:")
+print('Myname is',MyNamy,'i am',MyAge,'and my faculty is',MyFaculty)
